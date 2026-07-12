f91074e153dcbfbe25a07e3c9c948d325910a99d
ndeen17 <nurudeeny17@gmail.com>
2026-07-12 16:03:08 -0700

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,24 @@
 import Hero from '../sections/Hero';
+import Tools from '../sections/Tools';
 import Expertise from '../sections/Expertise';
 import DailyWork from '../sections/DailyWork';
 import EmploymentHistory from '../sections/EmploymentHistory';
 import Education from '../sections/Education';
 import Certifications from '../sections/Certifications';
 import Clients from '../sections/Clients';
-import Tools from '../sections/Tools';
 import Contact from '../sections/Contact';
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Tools />
       <Expertise />
       <DailyWork />
       <EmploymentHistory />
       <Education />
       <Certifications />
       <Clients />
-      <Tools />
       <Contact />
     </>
   );
